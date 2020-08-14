1966495a2c1a1a42a97db1db62b2ec1873b8e0d9
Galudisu <galudisu@gmail.com>
2020-08-14 16:16:26 +0800

    chapter3 fixed some issue

--- part1/chap3/src/main/scala/info/galudisu/_05_CSVToDatasetBookToDataframeApp.scala
+++ part1/chap3/src/main/scala/info/galudisu/_05_CSVToDatasetBookToDataframeApp.scala
@@ -1,11 +1,18 @@
 package info.galudisu
 
-import java.sql.Timestamp
-import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.chrono.ChronoLocalDate
+import java.time.format.{DateTimeFormatter, DateTimeFormatterBuilder}
+import java.time.temporal.ChronoField
+import java.util.Locale
 
 import com.typesafe.scalalogging.LazyLogging
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
 import org.apache.spark.sql.functions._
+
+import scala.util.{Failure, Success, Try}
+
+case class Book(id: Int, authorId: Int, link: String, title: String, releaseDate: LocalDate)
 
 /**
   * 在 dataset 和 dataframe 之间互转
@@ -16,6 +23,7 @@ object _05_CSVToDatasetBookToDataframeApp extends App with LazyLogging {
     .appName("CSV to dataframe to Dataset<Book> and back")
     .master("local")
     .getOrCreate()
+
   import spark.implicits._
 
   val df = spark.read.format("csv").option("inferSchema", "true").option("header", "true").load("data/books.csv")
@@ -23,39 +31,52 @@ object _05_CSVToDatasetBookToDataframeApp extends App with LazyLogging {
   df.show(5)
   df.printSchema()
 
-  case class Book(id: Int, authorId: Int, link: String, title: String, releaseDate: Option[java.sql.Timestamp])
+  val formatter = new DateTimeFormatterBuilder()
+    .appendPattern("M/d/")
+    .appendValueReduced(ChronoField.YEAR, 2, 4, 1900) // 年份从1900年开始。采取ISO 8601 记录方式
+    .toFormatter()
 
-  val parser = new SimpleDateFormat("M/d/yy")
+  val parseToDate: String => Option[LocalDate] = k =>
+    Try(LocalDate.parse(k, formatter)) match {
+      case Failure(_)     => None
+      case Success(value) => Some(value)
+  }
 
-  val bookDs = df.map(row => {
-    Book(
-      row.getAs[Int]("id"),
-      row.getAs[Int]("authorId"),
-      row.getAs[String]("link"),
-      row.getAs[String]("title"),
-      Option(row.getAs[String]("releaseDate")).map((r: String) => {
-        new Timestamp(parser.parse(r).getTime)
+  val parseToBook: Row => Option[Book] =
+    row => {
+      parseToDate(row.getAs[String]("releaseDate")).map(date => {
+        Book(
+          row.getAs[Int]("id"),
+          row.getAs[Int]("authorId"),
+          row.getAs[String]("link"),
+          row.getAs[String]("title"),
+          date
+        )
       })
-    )
-  })
+    }
+
+  val bookDs: Dataset[Book] = df.flatMap(parseToBook(_))
 
   logger.debug("*** Books are now in a dataset of books")
   bookDs.show(5, 17)
   bookDs.printSchema()
 
+  // DataSet[T] 转换为 DataFrame
   var df2 = bookDs.toDF()
 
+  // 从spark 3.0 开始内置提供对日期的支持。
+  //
   df2 = df2.withColumn(
     "releaseDateAsString",
-    concat(expr("releaseDate.year + 1900"),
+    concat(year($"releaseDate").plus(104), // 补充并更正新历年
            lit("-"),
-           expr("releaseDate.month + 1"),
+           date_format(add_months($"releaseDate", 1), "MM"),
            lit("-"),
-           df2.col("releaseDate.date"))
+           date_format($"releaseDate", "dd"))
   )
 
   df2 = df2
-    .withColumn("releaseDateAsDate", to_date(df2.col("releaseDateAsString"), "yyyy-MM-dd"))
+    .withColumn("releaseDateAsDate", to_date($"releaseDateAsString", "yyyy-MM-dd"))
     .drop("releaseDateAsString")
 
   logger.debug("*** Books are back in a dataframe")
